e542cc2051b826dc87e37852b550c867c1df6051
Sarah Li <sarcl@hotmail.com>
2018-06-26 19:37:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,11 +10,12 @@ class App < Sinatra::Base
   # This is a sample dynamic route.
   get "/hello/:name" do
     @user_name = params[:name]
-    "Hello #{name}!"
+    "Hello #{@user_name}!"
   end
 
   # Code your final two routes here:
   get '/goodbye/:name' do
+    @user_name = params[:name]
     "Goodbye, #{name}."
   end
 end
